86ee496cb6b7a8a8e5e3f7f6d1c036e16eec77c5
YazykovaDaria <yazykovadasva@gmail.com>
2025-03-17 22:51:14 +0300

    feat: stuff for styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -20,6 +17,7 @@ export default defineConfig({
       '@pages': path.resolve(__dirname, './src/pages'),
       '@entities': path.resolve(__dirname, './src/entities'),
       '@shared': path.resolve(__dirname, './src/shared'),
+      '@styles': path.resolve(__dirname, './src/shared/styles/index.scss'),
     },
   },
 });
