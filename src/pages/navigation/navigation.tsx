b01514a45f1f29efd45f0fe37f3fa6e851ba10d2
YazykovaDaria <yazykovadasva@gmail.com>
2025-04-09 14:33:13 +0300

    refactor: tag, style

--- src/pages/navigation/navigation.tsx
+++ src/pages/navigation/navigation.tsx
@@ -21,7 +21,7 @@ export const NavigationPage = () => {
       <NavigationWithPermissions
         navigationList={navigationList}
         checkPermission={checkHasUserPermission}
-      ></NavigationWithPermissions>
+      />
     </div>
   );
 };
